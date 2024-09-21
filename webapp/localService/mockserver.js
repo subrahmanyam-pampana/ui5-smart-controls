1cbdca420f8cb8f285a865bfc43c0bd4d8315997
Subrahmanyam Pampana <subrahmanyam.pampana@sap.com>
2024-09-21 14:29:13 +0530

    step1

---
+++ webapp/localService/mockserver.js
@@ -0,0 +1,28 @@
+sap.ui.define(["sap/ui/core/util/MockServer"], function (MockServer) {
+
+    return {
+        init:function(){
+            let oMockServer =  new MockServer({
+                rootUri: "/sap/opu/odata/sap/EPM_REF_APPS_PROD_MAN_SRV/"
+            })
+
+            // configure
+			MockServer.config({
+				autoRespond: true,
+				autoRespondAfter: 500
+			});
+
+            // simulate
+			var sPath = sap.ui.require.toUrl("sap/ui/demo/smartControls/localService");
+			oMockServer.simulate(sPath + "/metadata.xml", {
+				sMockdataBaseUrl: sPath + "/mockdata",
+				bGenerateMissingMockData: true
+			});
+
+			// start
+			oMockServer.start();
+        }
+    }
+
+
+});
