7ab3650a4d4b51deb76590c4ec46975cc51acc0d
Subrahmanyam Pampana <subrahmanyam.pampana@sap.com>
2024-09-21 16:36:20 +0530

    added smart table with varient management

--- webapp/Component.js
+++ webapp/Component.js
@@ -6,6 +6,12 @@ sap.ui.define([
 	return UIComponent.extend("sap.ui.demo.smartControls.Component", {
 		metadata: {
 			manifest: "json"
-		}
+		},
+		init: function () {
+            UIComponent.prototype.init.apply(this, arguments);
+        },
+        destroy: function () {
+                UIComponent.prototype.destroy.apply(this, arguments);
+        }
 	});
 });
